f5fe8c3fb7169a988f227d88b40df28157fdee1a
Maicon Diniz Filippsen <filippsen@blockie.org>
2018-06-15 20:05:06 +0200

    Rearrange and compress ContractTester code
    + Add setup procedure
    
    + Add error checking during setup
    
    * Provision style-mocha file
    
    * Move user-related actions to outer scope
    
    * Simplify constructor
    
    * Separate render code from setup

--- src/components/projecteditor/contracttester.js
+++ src/components/projecteditor/contracttester.js
@@ -16,7 +16,7 @@
 
 import { h, Component } from 'preact';
 // TODO: FIXME: pending style
-import style from './style';
+import style from './style-mocha';
 import SuperProvider from '../superprovider';
 import Web3 from 'web3';
 // TODO: FIXME: consider adding as part of the project (instead of dist)
@@ -32,47 +32,7 @@ export default class ContractTester extends Component {
         this.setState({account:0});
         this.contract_address="";
         this.contract_instance=null;
-        this.done=0;
-
-        //
-        // TODO: FIXME: move the following code out of constructor
-        const env=this.props.project.props.state.data.env;
-        const contract = this.dappfile.getItem("contracts", [{name: this.props.contract}]);
-        const src=contract.get('source');
-        const network=contract.get('network', env);
-        const endpoint=(this.props.functions.networks.endpoints[network] || {}).endpoint;
-        const web3=this._getWeb3(endpoint);
-
-        // Contract ABI
-        var contract_abi;
-        const abisrc=this._makeFileName(src, env, "abi");
-        this.props.project.loadFile(abisrc, (body) => {
-            if(body.status != 0) {
-                console.error("Unable to read contract ABI. Make sure the contract is deployed");
-                return;
-            }
-            contract_abi = body.contents;
-        });
-        var contract_interface = JSON.parse(contract_abi);
-
-        // Contract address
-        const addresssrc=this._makeFileName(src, env+"."+network, "address");
-        this.props.project.loadFile(addresssrc, (body) => {
-            if(body.status != 0) {
-                console.error("Unable to read contract address. Make sure the contract is deployed");
-                return;
-            }
-
-            this.contract_address = body.contents;
-        });
-
-        this.contract_instance = web3.eth.contract(contract_interface).at(this.contract_address);
-        mocha.setup('bdd');
     }
-
-    redraw = () => {
-        this.setState();
-    };
 
     _getWeb3=(endpoint)=>{
         var provider;
@@ -93,95 +53,164 @@ export default class ContractTester extends Component {
         return dir + "." + filename + "." + tag + "." + suffix;
     };
 
+    action_before() {
+        describe('User action before tests', function () {
+            it('should test a pre-test', function () {
+                if(false) {
+                    throw new Error("Unexpected error");
+                }
+            })
+        });
+    }
+
+    action_after() {
+        describe('User action after tests', function () {
+            it('should test a post-test', function () {
+                if(false) {
+                    throw new Error("Unexpected error");
+                }
+            })
+        });
+    }
+
+    check_data(instance, address, data_name, expected_value, done) {
+        it('matches data', function (done) {
+            instance._eth.call({to: address, data: data_name}, function(error, result) {
+                if(!error) {
+                    if(result !== expected_value) {
+                        done(new Error(result));
+                    } else {
+                        done();
+                    }
+                } else {
+                    done(new Error(error));
+                }
+            });
+        });
+
+    }
+
+    action_test=(instance, address, done)=> {
+        var check_data = this.check_data;
+        describe('User test action: check contract data', function (done) {
+            // TODO: FIXME: simplify check_data to minimize number of parameters 
+            //              (implicit instance and address)
+            // TODO: FIXME: change input data to proper signature (ABI)
+            // TODO: FIXME: change expected value output to human readable (decoded)
+            check_data(instance, address, "0xe21f37ce", "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000c48656c6c6f20576f726c64210000000000000000000000000000000000000000", done);
+        });
+    }
+
+    setup(action_before, action_test, action_after) {
+        const env=this.props.project.props.state.data.env;
+        const contract = this.dappfile.getItem("contracts", [{name: this.props.contract}]);
+        const src=contract.get('source');
+        const network=contract.get('network', env);
+        const endpoint=(this.props.functions.networks.endpoints[network] || {}).endpoint;
+        const web3=this._getWeb3(endpoint);
+
+        // Contract ABI
+        var contract_abi;
+        const abisrc=this._makeFileName(src, env, "abi");
+        this.props.project.loadFile(abisrc, (body) => {
+            if(body.status != 0) {
+                console.error("Unable to read contract ABI. Make sure the contract is deployed");
+                return 1;
+            }
+            contract_abi = body.contents;
+
+            if(!contract_abi) {
+                console.error("Unable to read contract ABI. Make sure the contract is deployed");
+                return 1
+            }
+
+            var contract_interface = JSON.parse(contract_abi);
+
+            // Contract address
+            const addresssrc=this._makeFileName(src, env+"."+network, "address");
+            this.props.project.loadFile(addresssrc, (body) => {
+                if(body.status != 0) {
+                    console.error("Unable to read contract address. Make sure the contract is deployed");
+                    return 2;
+                }
+
+                this.contract_address = body.contents;
+
+                if(!this.contract_address) {
+                    console.error("Unable to read contract address. Make sure the contract is deployed");
+                    return 2;
+                }
+
+                this.contract_instance = web3.eth.contract(contract_interface).at(this.contract_address);
+
+                var address = this.contract_address;
+                if(address) {
+                   mocha.setup('bdd');
+
+                    var instance = this.contract_instance;
+
+                    var contract_name=this.props.contract;
+                    describe(contract_name, function(done) {
+                        before(function (done) {
+                            console.log('Preparing test suite...');
+                            this.timeout(0);
+
+                            if(action_before) {
+                                action_before();
+                            }
+
+                            done();
+                        });
+
+                        it('Running test suite...', function (done) {
+                            console.log('Running test suite...');
+                            action_test(instance, address, done);
+                            done();
+                        });
+
+                        after(function (done) {
+                            console.log('Finalizing test suite...');
+
+                            if(action_after) {
+                                action_after();
+                            }
+
+                            done();
+                        });
+                    });
+
+                    mocha.checkLeaks();
+                    mocha.run();
+                    return 0;
+                }
+            });
+        });
+    };
+
+    redraw = () => {
+        this.setState();
+    };
+
     render() {
 
-        var address = this.contract_address;
+        // TODO: FIXME: read user tests (external)
+        var user_action_before_test = this.action_before;
+        var user_action_test = this.action_test;
+        var user_action_after_test = this.action_after;
+        var status = this.setup(user_action_before_test, user_action_test, user_action_after_test);
 
-        // TODO: FIXME: move the following code out of render loop
-        if(address && this.done==0) {
-            //this.done = 1;
-
-            var instance = this.contract_instance;
-
-            function action_before() {
-                describe('hook: before user tests', function () {
-                    it('should test a pre-test', function () {
-                        if(false) {
-                            throw new Error("Unexpected error");
-                        }
-                    })
-                });
-            }
-
-            function action_after() {
-                describe('hook: after user tests', function () {
-                    it('should test a post-test', function () {
-                        if(false) {
-                            throw new Error("Unexpected error");
-                        }
-                    })
-                });
-            }
-
-            function check_data(data_name, expected_value, done) {
-                it('matches data', function (done) {
-                    instance._eth.call({to: address, data: data_name}, function(error, result) {
-                        if(!error) {
-                            if(result !== expected_value) {
-                                done(new Error(result));
-                            } else {
-                                done();
-                            }
-                        } else {
-                            done(new Error(error));
-                        }
-                    });
-                });
-
-            }
-
-            function action_test(done){
-                // TODO: FIXME: read user tests (external)
-                describe('Check contract data', function (done) {
-                    // TODO: FIXME: change input data to proper signature (ABI)
-                    // TODO: FIXME: change expected value output to human readable (decoded)
-                    check_data("0xe21f37ce", "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000c48656c6c6f20576f726c64210000000000000000000000000000000000000000", done);
-                });
-            }
-
-            var contract_name=this.props.contract;
-            describe(contract_name, function(done) {
-                before(function (done) {
-                    console.log('Preparing test suite...');
-                    this.timeout(0);
-
-                    if(action_before) {
-                        action_before();
-                    }
-
-                    done();
-                });
-
-                it('Running test suite...', function (done) {
-                    console.log('Running test suite...');
-                    action_test(done);
-                    done();
-                });
-
-                after(function (done) {
-                    console.log('Finalizing test suite...');
-
-                    if(action_after) {
-                        action_after();
-                    }
-
-                    done();
-                });
-            });
-
-            mocha.checkLeaks();
-            mocha.run();
-
+        // Render results
+        if(status == 1) {
+            return (
+                    <div class="scrollable-y" id={this.id+"_scrollable"}>
+                    <h1>Unable to read contract ABI. Make sure the contract is deployed</h1>
+                    </div>);
+        } else if(status == 2) {
+            return (
+                    <div class="scrollable-y" id={this.id+"_scrollable"}>
+                    <h1>Unable to read contract address. Make sure the contract is deployed</h1>
+                    </div>);
+        } else {
             return (
                     <div class="scrollable-y" id={this.id+"_scrollable"}>
                     <div id="mocha">
