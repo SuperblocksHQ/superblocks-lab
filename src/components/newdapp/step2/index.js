28d5cfb931b059580236ec372d54af135cb8c9a3
Waheed Akhtar <waheedakhtar694@gmail.com>
2018-09-03 18:06:08 +0500

    grid responsive issue fixture

--- src/components/newdapp/step2/index.js
+++ src/components/newdapp/step2/index.js
@@ -14,7 +14,7 @@ TemplateCategory.protoTypes = {
 
 const GridLayout = ({ templates, onTemplateSelected, templateSelectedId, categorySelectedId } = props) => (
     <div class={style.gridLayout} style={{paddingLeft: '10px'}}>
-        <div id="mainContent" className="container" style={{display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridGap: '10px', gridAutoRows: 'minMax(100px, auto)'}}>
+        <div id="mainContent" className="container" style={{display: 'grid', gridTemplateColumns: 'repeat(2, 0fr)', gridGap: '10px', gridAutoRows: 'minMax(100px, auto)'}}>
         { templates
             .filter((template) => template.categories.indexOf(categorySelectedId) > -1)
             .map((template) => (
@@ -39,10 +39,12 @@ GridLayout.protoTypes = {
 
 const TemplateLayout = ({ image, name, description, selected, onTemplateSelected } = props) => (
     <div onClick={onTemplateSelected} class={classNames([style.templateLayout], { [style.selected]: selected })}
-         style={{display:'flex', alignItems:'center', justifyContent:'center', flexDirection: 'column'}}>
+         style={{alignItems:'center', justifyContent:'center', flexDirection: 'column', display:'grid'}}>
         <img src={image} width="300"/>
+        <div style={{height: '90px'}}>
         <div class={style.title}>{name}</div>
         <div class={style.description}>{description}</div>
+        </div>
     </div>
 );
 
