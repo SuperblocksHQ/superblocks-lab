251c4eff95a50c314768743b3530e33ce4fc8825
Javier Tarazaga <javier@superblocks.com>
2018-11-01 16:56:19 +0100

    B25. Fixed broken test

--- src/actions/projects.test.js
+++ src/actions/projects.test.js
@@ -1,12 +1,25 @@
 import * as actions from './projects';
+import ProjectItem from '../components/projecteditor/control/item/projectItem';
+jest.mock('../components/projecteditor/control/item/projectItem'); // ProjectItem is now a mock constructor
 
 describe('actions', () => {
+    beforeAll(() => {
+        ProjectItem.mockImplementation(() => {
+            return {
+                getInode: () => {
+                    return 123
+                }
+            }
+        });
+    });
+
     it('should create an action to select the projec to open when re-loading the app', () => {
-        const data = { props: { state: { data: { dir: 'project-name' } } } };
+        const projectItem = new ProjectItem();
+
         const expectedAction = {
             type: 'SELECT_PROJECT',
-            data: 'project-name',
+            data: 123,
         };
-        expect(actions.selectProject(data)).toEqual(expectedAction);
+        expect(actions.selectProject(projectItem)).toEqual(expectedAction);
     });
 });
