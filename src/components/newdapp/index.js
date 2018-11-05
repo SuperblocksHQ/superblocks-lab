e43d8672307a481761cf15e7e8e81ab91ed2b002
Javier Tarazaga <javier@superblocks.com>
2018-11-05 17:41:45 +0100

    Fixed T34 and moved panel open/close logic to Redux

--- src/components/newdapp/index.js
+++ src/components/newdapp/index.js
@@ -14,121 +14,17 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { Component } from 'react';
-import Proptypes from 'prop-types';
-import SelectedTemplate from './selectTemplate';
-import ProjectDetails from './projectDetails';
-import Templates from '../templates';
-import DappfileItem from '../projecteditor/control/item/dappfileItem';
+import { connect } from 'react-redux';
+import NewDapp from './NewDapp';
+import { closeAllPanels } from '../../actions/view';
 
-export default class NewDapp extends Component {
-    state = {
-        selectedTemplate: null,
-        currentStep: 1,
-    };
-
-    onCloseClickHandle = () => {
-        this.closeModal();
-    };
-
-    onTemplateSelectedHandle = selectedTemplate => {
-        this.setState({
-            currentStep: 2,
-            selectedTemplate: selectedTemplate,
-        });
-    };
-
-    onProjectDetailsDone = projectInfo => {
-        const name = projectInfo.name;
-        const title = projectInfo.title;
-
-        const files = this.state.selectedTemplate.files;
-
-        // Try to decode the `/dappfile.json`.
-        var dappfile;
-        try {
-            dappfile = JSON.parse(
-                files['/'].children['dappfile.json'].contents
-            );
-        } catch (e) {
-            dappfile = DappfileItem.getDefaultDappfile();
-            files['/'].children['dappfile.json'] = { type: 'f' };
+const mapDispatchToProps = (dispatch) => {
+    return {
+        closeAllPanels: () => {
+            dispatch(closeAllPanels())
         }
-        dappfile.project.info.name = name;
-        dappfile.project.info.title = title;
-        files['/'].children['dappfile.json'].contents = JSON.stringify(
-            dappfile
-        );
-
-        this.props.backend.createProject(files, status =>
-            this.props.cb(status)
-        );
-
-        this.closeModal();
-    };
-
-    closeModal() {
-        this.props.functions.modal.cancel();
-    }
-
-    pop = () => {
-        this.setState({
-            currentStep: this.state.currentStep - 1,
-        });
-    };
-
-    render() {
-        let step;
-        switch (this.state.currentStep) {
-            case 1:
-                step = (
-                    <SelectedTemplate
-                        categories={Templates.categories}
-                        templates={Templates.templates}
-                        onTemplateSelected={this.onTemplateSelectedHandle}
-                        onBackPress={this.closeModal}
-                        onCloseClick={this.onCloseClickHandle}
-                    />
-                );
-                break;
-            case 2:
-                step = (
-                    <ProjectDetails
-                        projectName={
-                            this.state.projectInfo
-                                ? this.state.projectInfo.name
-                                : ''
-                        }
-                        projectTitle={
-                            this.state.projectInfo
-                                ? this.state.projectInfo.title
-                                : ''
-                        }
-                        onProjectDetailsDone={this.onProjectDetailsDone}
-                        onCloseClick={this.onCloseClickHandle}
-                        onBackClick={this.pop}
-                    />
-                );
-                break;
-            default:
-                step = (
-                    <SelectedTemplate
-                        categories={Templates.categories}
-                        templates={Templates.templates}
-                        onTemplateSelected={this.onTemplateSelectedHandle}
-                        onBackPress={this.closeModal}
-                        onCloseClick={this.onCloseClickHandle}
-                    />
-                );
-                break;
-        }
-
-        return <div>{step}</div>;
     }
 }
 
-NewDapp.proptypes = {
-    modal: Proptypes.object.isRequired,
-    functions: Proptypes.object.isRequired,
-    cb: Proptypes.func.isRequired,
-};
+
+export default connect(null, mapDispatchToProps)(NewDapp);
--- src/components/projecteditor/index.js
+++ src/components/projecteditor/index.js
@@ -14,215 +14,27 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import style from './style.less';
-import Control from './control';
-import Panes from './panes';
-import TopBar from '../topbar';
-import BottomBar from '../bottomBar';
-import ContactContainer from '../contactContainer';
-import TransactionLogPanel from '../blockexplorer/transactionlogPanel';
-import { IconTransactions, IconClose } from '../icons';
+import { connect } from 'react-redux';
+import ProjectEditor from './ProjectEditor';
+import { openTransactionsHistoryPanel, hideTransactionsHistoryPanel, toggleTransactionsHistoryPanel } from '../../actions/view';
+import { getopenTransactionsHistoryPanel } from '../../selectors/view';
 
-export default class ProjectEditor extends Component {
-    state = {
-        controlPanelWidth: 280,
-        draggin: false,
-        showTransactions: false,
-    }
+const mapStateToProps = state => ({
+    displayTransactionsPanel: getopenTransactionsHistoryPanel(state),
+});
 
-    constructor(props) {
-        super(props);
-
-        this.props.router.register('main', this);
-
-        // Mute defalt ctrl-s behavior.
-        window.addEventListener(
-            'keydown',
-            function(e) {
-                if (
-                    e.keyCode == 83 &&
-                    (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey)
-                ) {
-                    e.preventDefault();
-                }
-            },
-            false
-        );
-    }
-
-    // we could get away with not having this (and just having the listeners on
-    // our div), but then the experience would be possibly be janky. If there's
-    // anything w/ a higher z-index that gets in the way, then you're toast,
-    // etc.
-    componentDidUpdate(props, state) {
-        if (this.state.dragging && !state.dragging) {
-            document.addEventListener('mousemove', this.onMouseMove);
-            document.addEventListener('mouseup', this.onMouseUp);
-        } else if (!this.state.dragging && state.dragging) {
-            document.removeEventListener('mousemove', this.onMouseMove);
-            document.removeEventListener('mouseup', this.onMouseUp);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        toggleTransactionsHistoryPanel: () => {
+            dispatch(toggleTransactionsHistoryPanel())
+        },
+        openTransactionsHistoryPanel: () => {
+            dispatch(openTransactionsHistoryPanel())
+        },
+        closeTransactionsHistoryPanel: () => {
+            dispatch(closeTransactionsHistoryPanel())
         }
-    }
-
-    redraw = all => {
-        if (this.props.router.control) {
-            this.props.router.control.redraw();
-        }
-        if (this.props.router.app) {
-            this.props.router.app.redraw(all);
-        }
-        if (this.props.router.panes) {
-            this.props.router.panes.redraw(all);
-        }
-    };
-
-    onMouseMove = e => {
-        e.stopPropagation();
-        e.preventDefault();
-        const { dragging } = this.state;
-        const maxSize = screen.width * 0.35;
-        if (!dragging) return;
-        if (e.pageX < maxSize) {
-            this.setState({
-                controlPanelWidth: e.pageX
-            });
-        } else if (e.pageX >= maxSize) {
-            return null;
-        } else {
-            this.onMouseUp(e);
-        }
-    }
-
-    onMouseUp = e => {
-        e.stopPropagation();
-        e.preventDefault();
-
-        this.setState({ dragging: false });
-    };
-
-    onMouseDown = e => {
-        e.stopPropagation();
-        e.preventDefault();
-
-        // only left mouse button
-        if (e.button !== 0) return;
-        this.setState({
-            dragging: true,
-        });
-    };
-
-    onShowHideTransactionsClicked = () => {
-        this.setState({
-            showTransactions: !this.state.showTransactions,
-        });
-        this.redraw(true);
-    };
-
-    onProjectSelectedHandle = () => {
-        this.setState({
-            showTransactions: false,
-        });
-    };
-
-    render() {
-        var endpoint = '';
-        var project;
-        if (this.props.router && this.props.router.control) {
-            project =
-                this.props.router.control &&
-                this.props.router.control.getActiveProject();
-            if (project) {
-                const network = project.getEnvironment();
-                endpoint = (
-                    this.props.functions.networks.endpoints[network] || {}
-                ).endpoint;
-            }
-        }
-        const { controlPanelWidth, showTransactions } = this.state;
-        return (
-            <div className={style.projecteditor} id="main_container">
-                <TopBar
-                    router={this.props.router}
-                    functions={this.props.functions}
-                    onProjectSelected={this.onProjectSelectedHandle}
-                />
-                <div style={{display: "flex", height: "100%"}}>
-                    <div
-                        key="main_control"
-                        id="main_control"
-                        className={style.control}
-                        style={{ width: controlPanelWidth }}
-                    >
-                        <Control
-                            router={this.props.router}
-                            functions={this.props.functions}
-                        />
-                        <ContactContainer />
-                    </div>
-                    <span
-                        className="resizer vertical"
-                        onMouseDown={this.onMouseDown}
-                    />
-                    <div style={{ position: "relative", width: "100%" }}>
-                        <div
-                            key="main_panes"
-                            id="main_panes"
-                            className={style.panescontainer}
-                        >
-                            <Panes
-                                router={this.props.router}
-                                functions={this.props.functions}
-                                isActionPanelShowing={showTransactions}
-                            />
-                            {showTransactions ? (
-                                <div className={style.actionContainer}>
-                                    <div className={style.header}>
-                                        <span className={style.title}>
-                                            Transactions History
-                                        </span>
-                                        <button
-                                            className={classNames([
-                                                style.icon,
-                                                'btnNoBg',
-                                            ])}
-                                            onClick={
-                                                this
-                                                    .onShowHideTransactionsClicked
-                                            }
-                                        >
-                                            <IconClose />
-                                        </button>
-                                    </div>
-                                    <TransactionLogPanel
-                                        router={this.props.router}
-                                    />
-                                </div>
-                            ) : null}
-                        </div>
-                        <div className={style.actionPanel}>
-                            <div className={style.actions}>
-                                <button
-                                    className={classNames([
-                                        style.action,
-                                        'btnNoBg',
-                                    ])}
-                                    onClick={this.onShowHideTransactionsClicked}
-                                >
-                                    <IconTransactions />
-                                    <span className={style.verticalText}>
-                                        Transactions
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
-                        <BottomBar
-                            endpoint={endpoint}
-                        />
-                </div>
-                </div>
-            </div>
-        );
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectEditor);
