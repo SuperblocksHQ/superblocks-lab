20323f257af4b1d28267320d7a5f8be5c6ebac91
Javier Tarazaga <javier@superblocks.com>
2019-01-03 15:32:36 +0100

    Minor ui fixes

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -126,9 +126,7 @@ export const IconMosaic = ({ ...props }) => (
 );
 
 // Top Bar
-export const IconTransactions = ({ ...props }) => (
-    <FaIcon icon={iconExchangeAlt} {...props} />
-);
+export const IconTransactions = ({ ...props }) => <svg fill="currentColor" preserveAspectRatio="xMidYMid meet" height="16px" width="16px" viewBox="0 0 512 512" style={{ verticalAlign: "middle" }} {...props}><path d="M388.9 266.3c-5.1-5-5.2-13.3-.1-18.4L436 200H211c-7.2 0-13-5.8-13-13s5.8-13 13-13h224.9l-47.2-47.9c-5-5.1-5-13.3.1-18.4 5.1-5 13.3-5 18.4.1l69 70c1.1 1.2 2.1 2.5 2.7 4.1.7 1.6 1 3.3 1 5 0 3.4-1.3 6.6-3.7 9.1l-69 70c-5 5.2-13.2 5.3-18.3.3zM123.1 404.3c5.1-5 5.2-13.3.1-18.4L76.1 338H301c7.2 0 13-5.8 13-13s-5.8-13-13-13H76.1l47.2-47.9c5-5.1 5-13.3-.1-18.4-5.1-5-13.3-5-18.4.1l-69 70c-1.1 1.2-2.1 2.5-2.7 4.1-.7 1.6-1 3.3-1 5 0 3.4 1.3 6.6 3.7 9.1l69 70c5 5.2 13.2 5.3 18.3.3z"/></svg>
 export const IconCollaborate = ({ ...props }) => (
     <IconImg src={'src="/static/img/icon-collaborate.png'} {...props} />
 );
