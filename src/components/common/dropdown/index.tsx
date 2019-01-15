99f304d44c78940e4e22efd69d74f173a2fe3239
Ostap Kravchuk <ostap@superblocks.com>
2019-01-15 16:10:54 +0100

    Add license headers

--- src/components/common/dropdown/index.tsx
+++ src/components/common/dropdown/index.tsx
@@ -1,3 +1,19 @@
+// Copyright 2018 Superblocks AB
+// 
+// This file is part of Superblocks Lab.
+// 
+// Superblocks Lab is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3 of the License.
+// 
+// Superblocks Lab is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
+
 import React from 'react';
 import { DropdownBasic } from './dropDownBasic';
 
@@ -29,7 +45,7 @@ export class DropdownContainer extends React.Component<Props, State> {
         this.ignoreClassName = 'ignore-react-onclickoutside' + Date.now();
     }
 
-    componentDidUpdate(prevProps) {
+    componentDidUpdate(prevProps: Props) {
         if (prevProps.showMenu !== this.props.showMenu) {
             this.setState({
                 menuVisible: this.props.showMenu
